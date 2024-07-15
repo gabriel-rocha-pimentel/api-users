7dbb2e5065c7e28370c9d6e34e6615cb1e034290
Gabriel Rocha Pimentel <gabrielrochapimentel.dev@gmail.com>
2024-07-15 15:59:10 -0300

    created api from users

---
+++ server.js
@@ -0,0 +1,57 @@
+import express from 'express';
+import { PrismaClient } from '@prisma/client';
+
+const app = express();
+const prisma = new PrismaClient();
+
+app.use(express.json());
+
+
+app.post('/users', async (request, response) => {
+	await prisma.user.create({
+		data: {
+			name: request.body.name,
+			lastname: request.body.lastname,
+			email: request.body.email,
+			password: request.body.password
+		}
+	});
+	
+	response.status(201).json(request.body);
+});
+
+app.get('/users', async (request, response) => {
+	const users = await prisma.user.findMany()
+	
+	response.status(200).json(users);
+});
+
+app.put('/users/:id', async (request, response) => {
+	await prisma.user.update({
+		where: {
+			id: request.params.id
+		},		
+		data: {
+			name: request.body.name,
+			lastname: request.body.lastname,
+			email: request.body.email,
+			password: request.body.password
+		}
+	});
+	
+	response.status(201).json(request.body);
+});
+
+app.delete('/users/:id', async (request, response) => {
+	await prisma.user.delete({
+		where: {
+			id: request.params.id
+		}
+	});
+
+	response.status(200).json({ message: "Usuário deletado com sucesso !"})
+});
+
+app.listen(3000);
+
+//database-password - 0NNJEQbhHJXAbbc4
