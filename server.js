e75b6386553ce5844d19678bde6290c50582c4c8
Gabriel Rocha Pimentel <gabrielrochapimentel.dev@gmail.com>
2024-07-18 23:39:48 -0300

    finalizado projeto de api

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 import { PrismaClient } from '@prisma/client';
 
 const app = express();
 const prisma = new PrismaClient();
 
 app.use(express.json());
-
+app.use(cors())
 
 app.post('/users', async (request, response) => {
 	await prisma.user.create({
